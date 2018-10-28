766236997810cb6d73e20e1eeaf328ae297058d8
5hito <beemans@foxmail.com>
2018-10-28 12:44:39 +0800

    0.1.6

---
+++ lib/Header/PTPrivacyViewController.h
@@ -0,0 +1,14 @@
+//
+//  PTPrivacyViewController.h
+//  PartnerSDK
+//
+//  Created by on 2018/9/26.
+//  Copyright © 2018年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTPrivacyViewController : UIViewController
+
+@end
+
